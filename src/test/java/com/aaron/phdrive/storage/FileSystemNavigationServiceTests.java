950fe3b5709dbe4d35ca4689196aada48b5e2d4e
Aaron Murillo <madenlordhobbies@gmail.com>
2022-05-27 11:36:17 +0200

    Added "this" before every "service" in FileSystemNavigationServiceTests

--- src/test/java/com/aaron/phdrive/storage/FileSystemNavigationServiceTests.java
+++ src/test/java/com/aaron/phdrive/storage/FileSystemNavigationServiceTests.java
@@ -2,6 +2,7 @@ package com.aaron.phdrive.storage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 
 import java.util.Random;
 
@@ -18,24 +19,24 @@ public class FileSystemNavigationServiceTests {
 	
 	@BeforeEach
 	public void init() {
-		properties.setLocation("target/dirs/" + Math.abs(new Random().nextLong()));
-		service = new FileSystemNavigationService(properties);
-		service.init();
+		this.properties.setLocation("target/dirs/" + Math.abs(new Random().nextLong()));
+		this.service = new FileSystemNavigationService(properties);
+		this.service.init();
 	}
 	
 	@Test
 	public void shouldCreateFolder() {
-		service.createFolder(DIR_PATH);
-		assertThat(service.load(DIR_PATH)).exists();
+		this.service.createFolder(DIR_PATH);
+		assertThat(this.service.load(DIR_PATH)).exists();
 	}
 	
 	@Test
 	@DisplayName("Should fail when trying to create already existing dir")
 	public void shouldCreateFolderShouldFailFileAlreadyExisting() {
-		service.createFolder(DIR_PATH);
+		this.service.createFolder(DIR_PATH);
 		
 		try {
-			service.createFolder(DIR_PATH);
+			this.service.createFolder(DIR_PATH);
 		} catch(Exception e) {
 			assertEquals(e.getClass(), StorageException.class);
 			assertEquals(e.getMessage(), "Folder already exists.");
