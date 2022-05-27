be0267354cb4866f5cf942d46da8743c7d1b3bd3
Aaron Murillo <madenlordhobbies@gmail.com>
2022-05-27 11:31:13 +0200

    Added test shouldCreateFolderShouldFailFileAlreadyExisting()

--- src/test/java/com/aaron/phdrive/storage/FileSystemNavigationServiceTests.java
+++ src/test/java/com/aaron/phdrive/storage/FileSystemNavigationServiceTests.java
@@ -1,10 +1,12 @@
 package com.aaron.phdrive.storage;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FileSystemNavigationServiceTests {
@@ -25,5 +27,18 @@ public class FileSystemNavigationServiceTests {
 	public void shouldCreateFolder() {
 		service.createFolder(DIR_PATH);
 		assertThat(service.load(DIR_PATH)).exists();
+	}
+	
+	@Test
+	@DisplayName("Should fail when trying to create already existing dir")
+	public void shouldCreateFolderShouldFailFileAlreadyExisting() {
+		service.createFolder(DIR_PATH);
+		
+		try {
+			service.createFolder(DIR_PATH);
+		} catch(Exception e) {
+			assertEquals(e.getClass(), StorageException.class);
+			assertEquals(e.getMessage(), "Folder already exists.");
+		}
 	}
 }
